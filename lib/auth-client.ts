f680f5ef853b81a7605b82a4af52cd3bd0448111
xzpjerry <xzpjerry@email.com>
2026-04-05 19:15:51 +0800

    feat: add email-password auth client plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,9 @@
 import { createAuthClient } from "better-auth/client"
-import { emailOTPClient } from "better-auth/client/plugins"
+import { emailOTPClient, emailPasswordClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-  plugins: [emailOTPClient()],
+  plugins: [
+    emailOTPClient(),
+    emailPasswordClient(),
+  ],
 })
